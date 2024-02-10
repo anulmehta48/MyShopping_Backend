484e70e1eab8d98321e4f5623e0ff67f7fef68ea
anulmehta48 <anulmehta1848@gmail.com>
2024-02-10 16:21:58 +0530

    product

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,6 +1,6 @@
 import productModel from "../models/productModel.js";
 import uploadOncloudinary from "../utils/cloudinary.js";
-import { isValid, nameFormet, validateSizes } from "../utils/validation.js";
+import { isValid, isValidObjectId, isValidRequestBody, nameFormet, validateSizes } from "../utils/validation.js";
 
 export const createproduct = async (req, res) => {
   try {
@@ -77,4 +77,134 @@ export const createproduct = async (req, res) => {
   } catch (error) {
     res.status(500).send({ status: false, error: error.message });
   }
-};
+}
+
+export const getproduct=async (req,res)=>{
+  try {
+    const productId = req.params.productId
+    if (!isValidObjectId(productId)){
+      return res.status(400).send({ status: false, message: "Provide valid product id" })
+    } 
+    const productDetails = await productModel.findOne({ _id: productId, isDeleted: false })
+    if (!productDetails){
+     return res.status(404).send({ status: false, msg: "product is not found" })
+    }
+    return res.status(200).send({ status: true, message: "Success", data: productDetails })
+  }
+  catch (error) {
+    return res.status(500).send({ status: false, message: error.message })
+  }
+}
+
+export const updateProduct = async function (req, res) {
+  try {
+    const productId = req.params.productId;
+    const data = req.body;
+   
+    if (!isValidObjectId(productId)){
+      return res.status(400).send({ status: false, message: "Provide valid product id" })
+    } 
+    const productDetails = await productModel.findOne({ _id: productId, isDeleted: false })
+    if (!productDetails){
+      return res.status(404).send({ status: false, msg: "product is not found" })
+    }
+    const { title, description, price, currencyId, currencyFormat, isFreeShipping, productImage, style, availableSizes, installments, isDeleted } = data
+  
+    if (Object.keys(data).length == 0){
+      return res.status(400).send({ status: false, message: "please provide data" });
+    } 
+    if (title){
+      if (!isValid(title)) {
+        return res.status(400).send({ status: false, message: "can't empty title field" })
+      }
+      if (!nameFormet(title)){
+        return res.status(400).send({ status: false, message: "title  must be alphabetic characters" })
+      }
+      let isTitlePresent = await productModel.findOne({ title })
+      if (isTitlePresent) {
+          return res.status(400).send({ status: false, message: "title is already present" })
+      }
+    }
+  
+    if (description){
+      if (!isValid(description)){
+        return res.status(400).send({ status: false, message: "description  must be alphabetic characters" })
+      } 
+    }
+    if (price){
+      if (!/^[0-9 .]+$/.test(price)){
+        return res.status(400).send({ status: false, message: "price must be in numeric" })
+       }
+    }
+    if (currencyId){
+      if (currencyId != "INR"){
+        return res.status(400).send({ status: false, message: "currency Id must be INR" })
+      }
+    }
+    if (currencyFormat){
+      if(currencyFormat != "₹") {
+        return res.status(400).send({ status: false, message: "currency formet must be ₹ " })
+      }
+    }
+    if(style){
+      if(!isValid(style)) {
+        return res.status(400).send({ status: false, message: "style must be alphabetic characters" })
+      }
+    }
+    if (isFreeShipping){
+      if (!typeof (isFreeShipping) == Boolean) {
+        return res.status(400).send({ status: false, message: "isFreeShipping must be  Boolean" })
+      }
+    }
+    if (isDeleted){
+      return res.status(400).send({ status: false, message: "can't update isDeleted field" })
+    }
+    if(availableSizes){
+      const validation = validateSizes(availableSizes);
+      if (!validation.validate) {
+        return res.status(400).send({ status: false, message: `availableSizes should have only these Sizes ['S' || 'XS' || 'M' || 'X' || 'L' || 'XXL' || 'XL']` });
+      }
+      data.availableSizes = validation.sizeArray
+    }
+    if(installments){
+      if (isNaN(installments)){
+        return res.status(400).send({ status: false, message: "installments should be number only" })
+      }
+    }
+    if (req.file) {
+      const productPath = req.file.path;
+      if (!productPath) {
+        return res.status(400).send({ status: false, message: "Profile path is required" });
+      }
+      const productlink = await uploadOncloudinary(productPath);
+      if (!productlink) {
+        return res.status(400).send({ status: false, message: "Profile link is required" });
+      }
+      data.productImage = productlink.url;
+    }
+    const updatedData = await productModel.findOneAndUpdate({ _id: productId }, data, { new: true });
+    return res.status(200).send({ status: true, message: "product details updated", data: updatedData, });
+  } catch (error) {
+    return res.status(500).send({ status: false, message: error.message })
+  }
+}
+
+export const deleteproduct = async function (req, res) {
+  try {
+    const productId = req.params.productId
+
+    if (!isValidRequestBody(productId)){
+      return res.status(400).send({ status: false, message: "check productId...." })
+    }
+    const check = await productModel.findOne({ _id: productId, isDeleted: false })
+    if (!check) {
+      return res.status(404).send({ status: false, message: "product not found" })
+    }
+    let deletes = await productModel.findOneAndUpdate({_id: productId}, {$set: {isDeleted: true,deletedAt: Date.now()}}, {new: true})
+    return res.status(200).send({ status: true, message: "product Deleted Successfully....." })
+
+  } catch (error) {
+    return res.status(500).send({status: false,message: error.message})
+  }
+}
+
--- src/routers/router.js
+++ src/routers/router.js
@@ -2,7 +2,7 @@ import express from "express";
 import { createUser, getUserDetails, loginUser, updateUser } from "../controllers/userController.js";
 import { uploadProductImage, uploadProfileImage } from "../middlewares/multer.js";
 import { authentication, authorization } from "../middlewares/auth.js";
-import { createproduct } from "../controllers/productController.js";
+import { createproduct, deleteproduct, getproduct, updateProduct } from "../controllers/productController.js";
 const router = express.Router();
 router.get("/", (req, res) => {
   res.status(200).send({ message: "My Api Running fine" });
@@ -12,8 +12,11 @@ router.post("/createuser",uploadProfileImage,createUser)
 router.post("/login",loginUser)
 router.get("/getuser/:userId",authentication,getUserDetails)
 router.put("/updateuser/:userId",authentication,authorization,uploadProfileImage,updateUser)
-router.post("/createproduct",uploadProductImage,createproduct)
 
+router.post("/createproduct",uploadProductImage,createproduct)
+router.get("/getproduct/:productId",getproduct)
+router.put("/updateproduct/:productId",uploadProductImage,updateProduct)
+router.delete("/deleteproduct/:productId",deleteproduct)
 
 router.all("/*", (req, res) => {
   res.status(400).send({ status: false, message: "Make Sure Your Endpoint is Correct!" });
